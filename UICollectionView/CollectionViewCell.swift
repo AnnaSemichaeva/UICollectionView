9f1a6699fe67b3cd2158928c37471bde7ffacacd
Anna Semichaeva <tdhnss@gmail.com>
2022-03-14 21:37:24 +0300

    Initial commit

---
+++ UICollectionView/CollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewCell.swift
+//  UICollectionView
+//
+//  Created by macuser on 3/14/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+   
+    @IBOutlet private weak var countryNameLabel: UILabel!
+    
+    func configure(with countryName: String) {
+        countryNameLabel.text = countryName
+    }
+    
+    
+}
---
+++ UICollectionView/CollectionViewController.swift
@@ -0,0 +1,43 @@
+//
+//  CollectionViewController.swift
+//  UICollectionView
+//
+//  Created by macuser on 3/14/22.
+//
+
+import UIKit
+
+
+
+class CollectionViewController: UICollectionViewController {
+
+    let dataSource: [String] = ["United Kindrom", "Japan", "China", "Brazil", "India", "Russia", "USA"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return dataSource.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        var cell = UICollectionViewCell()
+        
+        if let countryCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? CollectionViewCell {
+            
+            countryCell.configure(with: dataSource[indexPath.row])
+            
+            cell = countryCell
+        }
+        
+        
+        return cell
+        
+    }
+    
+   
+
+}
